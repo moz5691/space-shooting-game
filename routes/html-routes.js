70f2dce0ac850f998241f1eceb2d4d317031a13b
student <student@Cjs-MacBook-Pro.local>
2018-12-07 18:25:11 -0500

    Rename index html to start html, test html to index html, added html route to /start for start html, redirect after game over to /start

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,16 @@
 const path = require("path");
 
 module.exports = function(app) {
-  // Sends homepage to client
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-  // Sends test to client
   app.get("/game", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/game.html"));
   });
-  // Sends test to client
   app.get("/intro", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/intro.html"));
+  });
+  app.get("/start", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/start.html"));
   });
 };
