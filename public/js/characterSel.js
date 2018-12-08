93e008bf67995a588aafd5e41ffc972737420087
student <student@Cjs-MacBook-Pro.local>
2018-12-08 07:03:40 -0500

    eslint js and added player one session storage in intro text

--- public/js/characterSel.js
+++ public/js/characterSel.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 function characterPage() {
   $('#canvas').empty();
   const ajaxGetData = sessionStorage.getItem('user');
--- public/js/mainpage.js
+++ public/js/mainpage.js
@@ -1,39 +1,41 @@
+/* eslint-disable */
+
 function mainPage() {
-  $("#canvas").empty();
-  $("#canvas").append(
-    $("<div>").append(
-      $("<h1>").append(
-        $("<span>")
-          .text(`Blue `)
-          .addClass("mainTitle blue-text blueTitle")
-          .css("float", "left")
-          .css("font-family", "'Press Start 2P', cursive"),
-        $("<span>")
-          .text(`  Vs  `)
-          .addClass("mainTitle white-text vsTitle")
-          .css("float", "left")
-          .css("font-family", "'Press Start 2P', cursive"),
-        $("<span>")
-          .text(` Red`)
-          .addClass("mainTitle red-text redTitle")
-          .css("float", "left")
-          .css("font-family", "'Press Start 2P', cursive")
+  $('#canvas').empty();
+  $('#canvas').append(
+    $('<div>').append(
+      $('<h1>').append(
+        $('<span>')
+          .text('Blue ')
+          .addClass('mainTitle blue-text blueTitle')
+          .css('float', 'left')
+          .css('font-family', "'Press Start 2P', cursive"),
+        $('<span>')
+          .text('  Vs  ')
+          .addClass('mainTitle white-text vsTitle')
+          .css('float', 'left')
+          .css('font-family', "'Press Start 2P', cursive"),
+        $('<span>')
+          .text(' Red')
+          .addClass('mainTitle red-text redTitle')
+          .css('float', 'left')
+          .css('font-family', "'Press Start 2P', cursive"),
       ),
-      $("<br/>"),
-      $("<p>")
-        .addClass("enter")
-        .text("Press Enter to Start")
-        .css("font-size", "30px")
-        .addClass("center-align startText blinking")
+      $('<br/>'),
+      $('<p>')
+        .addClass('enter')
+        .text('Press Enter to Start')
+        .css('font-size', '30px')
+        .addClass('center-align startText blinking')
         .append(
-          $("<i>")
-            .addClass("medium material-icons iconGo blinking")
-            .text("exit_to_app")
-        )
-    )
+          $('<i>')
+            .addClass('medium material-icons iconGo blinking')
+            .text('exit_to_app'),
+        ),
+    ),
   );
 }
-$(document).keypress(function(e) {
+$(document).keypress((e) => {
   if (e.keyCode === 13) {
     e.preventDefault();
     characterPage();
