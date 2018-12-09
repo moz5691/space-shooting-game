b2832c7ac7b111f2a2679783061dc9ad1ae25e02
Yoonsub <yoonsub90@gmail.com>
2018-12-08 19:04:56 -0500

    return fixed

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
   app.get("/intro", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/intro.html"));
   });
-  app.get("/index1", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index1.html"));
+  app.get("/start", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/start.html"));
   });
 };
